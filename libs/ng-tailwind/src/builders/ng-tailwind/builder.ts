4cb67c6e3214b4a16c0af93db9dad3fda8d1bd0a
Sebastian Guevara <info@sebasg.dev>
2020-03-05 20:57:59 -0500

    feat(ng-tailwind): adding builder for build mode

--- libs/ng-tailwind/src/builders/ng-tailwind/builder.ts
+++ libs/ng-tailwind/src/builders/ng-tailwind/builder.ts
@@ -3,36 +3,54 @@ import {
   BuilderOutput,
   createBuilder
 } from '@angular-devkit/architect';
-import { Observable, of, noop } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable, of, from } from 'rxjs';
+import { map, tap, catchError } from 'rxjs/operators';
 import { NgTailwindBuilderSchema } from './schema';
 import * as build from 'ng-tailwindcss/lib/build';
 import * as watch from 'ng-tailwindcss/lib/watch';
+import { render } from 'node-sass';
+import { normalize, resolve } from 'path';
+import { existsSync } from 'fs';
 
 export function runBuilder(
   options: NgTailwindBuilderSchema,
   context: BuilderContext
 ): Observable<BuilderOutput> {
   context.logger.info('Builder start for ng-tailwind', { options });
-  try {
-    this.selectMode(options);
-    context.logger.info('Builder ran for ng-tailwind');
-    return of({ success: true });
-  } catch (e) {
-    return of({ success: false });
-  }
+  return from(selectMode(options)).pipe(
+    map(() => ({ success: true })),
+    tap(() => context.logger.warn('Mutation Tests ran successfully')),
+    catchError(e => {
+      context.logger.error('Failed to ran mutation tests', e);
+      return of({ success: false });
+    })
+  );
+  // try {
+  //   selectMode(options);
+  //   context.logger.info('Builder ran for ng-tailwind');
+  // } catch (e) {
+  //   return of({ success: false });
+  // }
 }
 
 function selectMode(options: NgTailwindBuilderSchema) {
-  switch (options.mode) {
-    case "watch": {
-      return watch({ configPath: options.configPath });
+  return new Promise((res, rej) => {
+    switch (options.mode) {
+      case 'watch': {
+        return watch({ configPath: options.configPath });
+      }
+      default: {
+        build({
+          purgeFlag: options.purge,
+          configPath: options.configPath
+        })
+          .then(response => res(response))
+          .catch(e => {
+            rej(e);
+          });
+      }
     }
-    default: {
-      console.warn(build.toString());
-      return build({ purgeFlag: options.purge, configPath: options.configPath });
-    }
-  }
+  });
 }
 
 export default createBuilder(runBuilder);
