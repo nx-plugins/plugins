7798411562fd183f83a3dd4ec6f1c1ecf4edd642
Sebastian Guevara <info@sebasg.dev>
2020-12-08 13:01:27 -0500

    chore(nx): update dependencies

--- libs/ng-tailwind/jest.config.js
+++ libs/ng-tailwind/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  name: 'ng-tailwind',
-  preset: '../../jest.config.js',
+  preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   coverageDirectory: '../../coverage/libs/ng-tailwind',
   globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
+  displayName: 'ng-tailwind',
 };
--- libs/npm/jest.config.js
+++ libs/npm/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  name: 'npm',
-  preset: '../../jest.config.js',
+  preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   coverageDirectory: '../../coverage/libs/npm',
   globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
+  displayName: 'npm',
 };
